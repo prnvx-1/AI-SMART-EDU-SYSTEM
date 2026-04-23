a26e11bc840f9a513130c18d025a39024bdda900
prnvx-1 <naghulpranav951@gmail.com>
2026-04-23 13:07:04 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smarteducation.app',
+  appName: 'Smart Education',
+  webDir: 'out'
+};
+
+export default config;
